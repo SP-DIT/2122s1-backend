b712f0a6af371d71fdef1b9d4f4da452e6a62442
Jeremiah Ang <s43500@sp.edu.sg>
2021-04-27 17:28:44 +0800

    Add drop instructions

--- database/init.sql
+++ database/init.sql
@@ -1,8 +1,11 @@
+DROP TABLE IF EXISTS queue_tab;
 CREATE TABLE queue_tab (
     id SERIAL primary key,
     served BOOLEAN not null default false
 );
 
+DROP INDEX IF EXISTS timestamp_key;
+DROP TABLE IF EXISTS error_tab;
 CREATE TABLE error_tab (
     id SERIAL primary key,
     timestamp INT not null,
